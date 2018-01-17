efa008ba00feb9d4779016ff40dfa5b8259ffde7
Oren Mishali <oren.mishali@gmail.com>
2018-01-17 11:22:43 +0200

    Refactoring

--- src/test/java/apps/jbsmekorot/TestJbsManipulations.java
+++ src/test/java/apps/jbsmekorot/TestJbsManipulations.java
@@ -13,14 +13,13 @@ import static org.junit.Assert.*;
 /**
  * Created by omishali on 08/01/2018.
  */
-public class TestManipulations {
+public class TestJbsManipulations {
     SpannedDocument doc;
-    private String text1 = "כי מקרא מלא שהביאוהו זכרונם לברכה לראיתם (ישעיה ט, טז) על כן על בחוריו לא ישמח ה' וגו'";
-    private String text2 = "בראשית ברא אלקים את השמים ואת הארץ 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 בראשית ברא אלקים";
-    private String text3 = "להבדיל בין הטמא ובין הטהור";
+
     @Test
     public void testShemAdnutManipulation() {
-        doc = new SpannedDocument(text1, JbsMekorot.MINIMAL_PASUK_LENGTH, JbsMekorot.MAXIMAL_PASUK_LENGTH);
+        String text = "כי מקרא מלא שהביאוהו זכרונם לברכה לראיתם (ישעיה ט, טז) על כן על בחוריו לא ישמח ה' וגו'";
+        doc = new SpannedDocument(text, JbsMekorot.MINIMAL_PASUK_LENGTH, JbsMekorot.MAXIMAL_PASUK_LENGTH);
         JbsMekorot.findPsukim(doc);
 
         assertEquals("ישמח אדני", doc.getSpan(15, 16).getStringExtra(AddTextWithShemAdnut.ADNUT_TEXT));
@@ -30,7 +29,8 @@ public class TestManipulations {
 
     @Test
     public void testFilterTagsFromSpansManipulation() {
-        doc = new SpannedDocument(text2, JbsMekorot.MINIMAL_PASUK_LENGTH, JbsMekorot.MAXIMAL_PASUK_LENGTH);
+        String text = "בראשית ברא אלקים את השמים ואת הארץ 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 בראשית ברא אלקים";
+        doc = new SpannedDocument(text, JbsMekorot.MINIMAL_PASUK_LENGTH, JbsMekorot.MAXIMAL_PASUK_LENGTH);
         JbsMekorot.findPsukim(doc);
 
         assertEquals(getList("jbr:text-tanach-1-1-1"), doc.getSpan(0, 6).getSortedTags());
@@ -39,7 +39,8 @@ public class TestManipulations {
 
     @Test
     public void testRemoveNonSequentialTags() {
-        doc = new SpannedDocument(text3, JbsMekorot.MINIMAL_PASUK_LENGTH, JbsMekorot.MAXIMAL_PASUK_LENGTH);
+        String text = "להבדיל בין הטמא ובין הטהור";
+        doc = new SpannedDocument(text, JbsMekorot.MINIMAL_PASUK_LENGTH, JbsMekorot.MAXIMAL_PASUK_LENGTH);
         JbsMekorot.findPsukim(doc);
 
         // without the manipulation, we should detect here two psukim:
