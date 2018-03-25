d396866727ac5365a1874008ebfd54045828197b
VitaKop <vitakopilov@gmail.com>
2018-03-25 15:58:30 +0300

    file renaming

--- src/main/java/apps/jbsmekorot2spark/jbsmekorotApp.java
+++ src/main/java/apps/jbsmekorot2spark/jbsmekorotApp.java
@@ -2,9 +2,9 @@ package apps.jbsmekorot2spark;
 
 import org.apache.spark.sql.SparkSession;
 
-public class JbsMekorotApp {
+public class jbsmekorotApp {
     public static void main(String[] args){
-        SparkSession sparkSession = SparkSession.builder().appName("JbsMekorotApp").getOrCreate();
+        SparkSession sparkSession = SparkSession.builder().appName("jbsmekorotApp").getOrCreate();
         JbsSparkMekorot jbsMekorot = new JbsMekorot2(sparkSession);
         jbsMekorot.main(args);
     }
