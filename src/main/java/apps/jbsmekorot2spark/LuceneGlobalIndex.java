9e97992e5b8d0a874ef1dc8d7315783191305ca4
Or Asraf <jrspyser@gmail.com>
2018-04-10 13:37:04 +0300

    test : 1 global lucene index created before slaves are called.

---
+++ src/main/java/apps/jbsmekorot2spark/LuceneGlobalIndex.java
@@ -0,0 +1,9 @@
+package apps.jbsmekorot2spark;
+
+import apps.jbsmekorot.JbsTanachIndex;
+import apps.jbsmekorot.JbsTanachMaleIndex;
+
+public class LuceneGlobalIndex {
+    public static JbsTanachIndex tanach = new JbsTanachIndex();
+    public static JbsTanachMaleIndex tanachMale = new JbsTanachMaleIndex();
+}
